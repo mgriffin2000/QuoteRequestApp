1ae6c4028e480ccdfb67a0f10760fe754ea8eeac
Matthew Griffin <mgriffin2022@outlook.com>
2020-04-03 20:38:04 -0700

    Set up initial startup of the main class
    Initialized all the variables needed to build the HTML and tested building the correct HTML to be used for the GET.

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,8 +1,27 @@
 package App;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)  {
-        System.out.println("Hello World");
+        String dataSet = "function=TIME_SERIES_DAILY";       //type of info about the symbol that is selected
+        String apikey = "&apikey=D44LPNJ4W7DXPN2Y";         //alpha vantage apikey
+        String symbol = "&symbol=";                        //user entered stock symbol
+        String dataFormat = "&datatype=json";               //data set format in JSON
+        String baseURL = "https://www.alphavantage.co/query?";  //base URL
+        String finalURL;
+
+        Scanner in = new Scanner(System.in);        //declare scanner object
+        System.out.println("Please enter the stock ticker symbol you are interested in: ");
+        symbol = symbol + in.next().toUpperCase();           //user entered ticker symbol converted to upper case
+
+        //later add code here for user to enter other types of data and stuff that they want
+
+       finalURL = baseURL + dataSet + symbol + apikey + dataFormat;        //builds the final URL used for the GET
+
+        System.out.println(finalURL);
+
+
     }
 
 }
